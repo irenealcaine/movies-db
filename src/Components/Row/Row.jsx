395d617768360752ac93061ce6b6a299c3f3fbb7
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-04-06 15:51:59 +0200

    update 5 files

--- src/Components/Row/Row.jsx
+++ src/Components/Row/Row.jsx
@@ -1,8 +1,7 @@
-
 import ListItem from '../ListItem/ListItem'
 import React, { useEffect, useState } from 'react'
-import requests from '../../Requests'
 import axios from 'axios'
+import { SwiperSlide, Swiper } from 'swiper/react';
 
 const Row = ({ request }) => {
 
@@ -12,21 +11,31 @@ const Row = ({ request }) => {
     axios.get(request)
       .then((res) => {
         setItems(res.data.results)
-        console.log(items)
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
   }, [])
   return (
-    <div>
-      {items.map((item) => (
-        // <p key={item.id}>{item.title}</p>
-        <ListItem item={item} key={item.id} />
-      ))}
+    // <div>
+    //   {items.map((item) => (
+    //     // <p key={item.id}>{item.title}</p>
+    //     <ListItem item={item} key={item.id} />
+    //   ))}
 
 
-    </div>
+    // </div>
+    <Swiper>
+      <div>
+        {items.map((item) => (
+          <SwiperSlide>
+            <img style={{ height: "100px" }} src={`https://image.tmdb.org/t/p/original${item?.poster_path}`} alt={item?.title} />
+          </SwiperSlide>
+        ))}
+
+
+      </div>
+    </Swiper>
   )
 }
 
